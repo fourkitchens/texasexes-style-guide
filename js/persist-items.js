5758ef7564e5d1b66d3d503b377f5f941ce0c007
Joe Tower <joetower@gmail.com>
2014-09-05 13:38:26 -0500

    :eyes: Tried adding jQuery manually - unable to get jQuery to load in proper order. Refined Cover image styles

--- js/persist-items.js
+++ js/persist-items.js
@@ -1,43 +1,44 @@
+jQuery.noConflict();
 (function ($) {
     'use strict';
+
+    // DOM Ready      
+    $(document).ready(function () {
+
     function UpdateTableHeaders() {
-       $(".persist-area").each(function() {
+       $('.persist-area').each(function() {
        
            var el             = $(this),
                offset         = el.offset(),
                scrollTop      = $(window).scrollTop(),
-               floatingHeader = $(".floatingHeader", this)
+               floatingHeader = $('.floatingHeader', this);
            
            if ((scrollTop > offset.top) && (scrollTop < offset.top + el.height())) {
                floatingHeader.css({
-                "visibility": "visible"
+                'visibility': 'visible'
                });
            } else {
                floatingHeader.css({
-                "visibility": "hidden"
+                'visibility' : 'hidden'
                });      
-           };
+           }
        });
     }
 
-    // DOM Ready      
-    $(function() {
-
        var clonedHeaderRow;
 
-       $(".persist-area").each(function() {
-           clonedHeaderRow = $(".persist-header", this);
+       $('.persist-area').each(function() {
+           clonedHeaderRow = $('.persist-header', this);
            clonedHeaderRow
              .before(clonedHeaderRow.clone())
-             .css("width", clonedHeaderRow.width())
-             .addClass("floatingHeader");
+             .css('width', clonedHeaderRow.width())
+             .addClass('floatingHeader');
              
        });
        
        $(window)
         .scroll(UpdateTableHeaders)
-        .trigger("scroll");
+        .trigger('scroll');
        
     });
 })(jQuery);
-    
---
+++ js/responsive-nav.js
@@ -0,0 +1,20 @@
+// Small handheld collpased navigation
+jQuery.noConflict();
+(function ($) {
+    'use strict';
+    $(document).ready(function () {
+    var pullNav                   = $('.pull-nav'),
+        menu                      = $('.navigation'), 
+        pullSearch                = $('.pull-search'),
+        search                    = $('.search'),
+        toggler                   = function(a){
+          $(this).toggleClass('active');
+          a.data.toggled.animate({
+            height: 'toggle',
+            opacity: 'toggle'
+            }, '500');
+        };      
+        pullNav.click({toggled : menu}, toggler);
+        pullSearch.click({toggled : search}, toggler);
+       });
+})(jQuery);
